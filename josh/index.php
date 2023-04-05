05e573c869a76c473afb5769a563981d6ed30631
Joshasup <hasupsong@gmail.com>
2023-04-04 17:36:42 -0700

    Update index.php

--- josh/index.php
+++ josh/index.php
@@ -197,7 +197,7 @@
                                 <h2 class="entry-title section-title">Saleblazers</h2>
 
                                 <div style="text-align: center;">
-                                <iframe src="https://www.youtube.com/watch?v=30CqiuRzJkg" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/vNSbXAKsIa0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                                 </div>
                                 <div id="portfolio-wrapper" class="relative">
                                     <div class="category-filter">
